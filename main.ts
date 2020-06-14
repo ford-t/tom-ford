8d1499a22660f071a40d617f07025711b35fe73c
ford-t <66909269+ford-t@users.noreply.github.com>
2020-06-14 16:47:51 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,290 @@
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile1 = img`
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+`
+    //% blockIdentity=images._tile
+    export const tile2 = img`
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+`
+    //% blockIdentity=images._tile
+    export const tile3 = img`
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+`
+    //% blockIdentity=images._tile
+    export const tile4 = img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`
+    //% blockIdentity=images._tile
+    export const tile5 = img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`
+    //% blockIdentity=images._tile
+    export const tile6 = img`
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+`
+}
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vx += -50
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vy = 200
+    mySprite.setImage(img`
+. . . . . . . . . . . . . . . . 
+. . d d . . e e . . d d . . . . 
+. . d d . . d d . . d d . . . . 
+. . d d . . d d . . d d . . . . 
+. . d d 9 9 9 9 9 9 d d . . . . 
+. . . . 9 9 9 9 9 9 . . . . . . 
+. . . . 9 9 9 9 9 9 . . . . . . 
+. . . . 9 9 9 9 9 9 . . . . . . 
+. . . . e e 5 e e e . . . . . . 
+. . . . 8 8 8 8 8 8 . . . . . . 
+. . . . 8 8 8 8 8 8 . . . . . . 
+. . . . 8 8 . . 8 8 . . . . . . 
+. . . . 8 8 . . 8 8 . . . . . . 
+. . . . 8 8 . . 8 8 . . . . . . 
+. . . . 8 8 . . 8 8 . . . . . . 
+. . . . f f . . f f . . . . . . 
+`)
+    pause(200)
+    mySprite.setImage(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . e e . . . . . . . . 
+. . . . . . d d . . . . . . . . 
+. . . . . . d d . . . . . . . . 
+. . d d 9 9 9 9 9 9 d d . . . . 
+. . d d 9 9 9 9 9 9 d d . . . . 
+. . d d 9 9 9 9 9 9 d d . . . . 
+. . d d 9 9 9 9 9 9 d d . . . . 
+. . . . e e 5 e e e . . . . . . 
+. . . . 8 8 8 8 8 8 . . . . . . 
+. . . . 8 8 8 8 8 8 . . . . . . 
+. . . . 8 8 . . 8 8 . . . . . . 
+. . . . 8 8 . . 8 8 . . . . . . 
+. . . . 8 8 . . 8 8 . . . . . . 
+. . . . 8 8 . . 8 8 . . . . . . 
+. . . . f f . . f f . . . . . . 
+`)
+})
+controller.left.onEvent(ControllerButtonEvent.Released, function () {
+    mySprite.vx += 50
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    game.over(false)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vy = -150
+    mySprite.setImage(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . e e . . . . . . . . 
+. . . . . . d d . . . . . . . . 
+. . . . . . d d . . . . . . . . 
+d d d d 9 9 9 9 9 9 d d d d . . 
+d d d d 9 9 9 9 9 9 d d d d . . 
+. . . . 9 9 9 9 9 9 . . . . . . 
+. . . . 9 9 9 9 9 9 . . . . . . 
+. . . . e e 5 e e e . . . . . . 
+. . . . 8 8 8 8 8 8 8 8 . . . . 
+. . . . 8 8 8 8 8 8 8 8 . . . . 
+. . . . 8 8 . . . . 8 8 . . . . 
+. . . . 8 8 . . . . 8 8 . . . . 
+. . . . 8 8 . . . . f f . . . . 
+. . . . 8 8 . . . . . . . . . . 
+. . . . f f . . . . . . . . . . 
+`)
+    pause(200)
+    mySprite.setImage(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . e e . . . . . . . . 
+. . . . . . d d . . . . . . . . 
+. . . . . . d d . . . . . . . . 
+. . d d 9 9 9 9 9 9 d d . . . . 
+. . d d 9 9 9 9 9 9 d d . . . . 
+. . d d 9 9 9 9 9 9 d d . . . . 
+. . d d 9 9 9 9 9 9 d d . . . . 
+. . . . e e 5 e e e . . . . . . 
+. . . . 8 8 8 8 8 8 . . . . . . 
+. . . . 8 8 8 8 8 8 . . . . . . 
+. . . . 8 8 . . 8 8 . . . . . . 
+. . . . 8 8 . . 8 8 . . . . . . 
+. . . . 8 8 . . 8 8 . . . . . . 
+. . . . 8 8 . . 8 8 . . . . . . 
+. . . . f f . . f f . . . . . . 
+`)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vx += 50
+})
+controller.right.onEvent(ControllerButtonEvent.Released, function () {
+    mySprite.vx += -50
+})
+let projectile: Sprite = null
+let mySprite: Sprite = null
+tiles.setTilemap(tiles.createTilemap(
+            hex`0a0008000808060707060606070708080606060606060606060606060707070606060607070606060606070706060606060606060606060606060606060606060a0a0103010a0a030a0a0a030101010303010a01`,
+            img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 
+`,
+            [myTiles.tile0,sprites.castle.tileGrass2,sprites.dungeon.hazardWater,sprites.castle.tileGrass1,sprites.castle.tileGrass3,sprites.castle.tileDarkGrass2,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,sprites.castle.tilePath8],
+            TileScale.Sixteen
+        ))
+mySprite = sprites.create(img`
+. . . . . . e e . . . . . . . . 
+. . . . . . d d . . . . . . . . 
+. . . . . . d d . . . . . . . . 
+. . d d 9 9 9 9 9 9 d d . . . . 
+. . d d 9 9 9 9 9 9 d d . . . . 
+. . d d 9 9 9 9 9 9 d d . . . . 
+. . d d 9 9 9 9 9 9 d d . . . . 
+. . d d 9 9 9 9 9 9 d d . . . . 
+. . . . e e 5 e e e . . . . . . 
+. . . . 8 8 8 8 8 8 . . . . . . 
+. . . . 8 8 8 8 8 8 . . . . . . 
+. . . . 8 8 . . 8 8 . . . . . . 
+. . . . 8 8 . . 8 8 . . . . . . 
+. . . . 8 8 . . 8 8 . . . . . . 
+. . . . 8 8 . . 8 8 . . . . . . 
+. . . . f f . . f f . . . . . . 
+`, SpriteKind.Player)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(4.5, 6))
+mySprite.ay = 500
+game.onUpdateInterval(500, function () {
+    info.changeScoreBy(1)
+})
+game.onUpdateInterval(500, function () {
+    projectile = sprites.createProjectileFromSide(img`
+. . . . . . . . f 4 4 . . . . . 
+. . . . . . . f . . 4 . . . . . 
+. . . . . . . f . . . . . . . . 
+. . . . . . f f f . . . . . . . 
+. . . . f f f f f f f . . . . . 
+. . . f f f f f f f f f . . . . 
+. . f f f f f f f f f f f . . . 
+. . f f f f f f f f f f f . . . 
+. f f f f f f f f f f f f f . . 
+. f f f f f f f f f f f f f . . 
+. f f f f f f f f f f f f f . . 
+. . f f f f f f f f f f f . . . 
+. . f f f f f f f f f f f . . . 
+. . . f f f f f f f f f . . . . 
+. . . . f f f f f f f . . . . . 
+. . . . . . f f f . . . . . . . 
+`, Math.randomRange(-100, 100), Math.randomRange(50, 100))
+})
